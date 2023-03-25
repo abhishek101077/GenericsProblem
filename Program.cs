1fb53d573975642a7d03e1da16b339972f60c6f0
Abhishek shukla <Abhishekgargshu786@gmail.com>
2023-03-25 21:44:52 +0530

    The programme file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,Welcome to Generics.");
+            Console.WriteLine("Welcome to the Generics Problem : ");
+            Findmax.FindMaxValue(20, 40, 50);
+            Console.ReadLine();
+        }
+    }
+}
